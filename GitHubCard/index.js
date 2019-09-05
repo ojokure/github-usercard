9392a20efa870d84cf7419d2dc5ffe7c105d45cf
oladimeji-ojo <ojokuredim@gmail.com>
2019-09-05 13:13:46 +0100

    first commit, step1

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,19 +1,21 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
-*/
+          
+*/ 
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
 
-   Skip to Step 3.
-*/
+      axios.get('https://api.github.com/users/ojokure')
 
+
+// /* Step 2: Inspect and study the data coming back, this is YOUR 
+//    github info! You will need to understand the structure of this 
+//    data in order to use it to build your component function 
+//    Skip to Step 3.
+// */
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -45,6 +47,16 @@ const followersArray = [];
 </div>
 
 */
+
+function createGitCard (postMan){
+
+const cardDiv = document.createElement('div');
+
+
+
+
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
